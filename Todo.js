26a6bac3eb2b77b4c5299e9c7e632e1358739d85
lama mahmoud <lamamahmoud213@gmail.com>
2022-06-02 23:17:27 +0300

    Sorting by time priority and NOT done

--- Todo.js
+++ Todo.js
@@ -7,10 +7,14 @@ class task {
 	this.time=time;
   }
 }
-$(document).ready(function(){
+
+function getTasks(isDone){
 	
+	$("#myList").empty();
 	
-	for(i=0;i<localStorage.length;i++)
+	if (isDone==-1)
+	{
+		for(i=0;i<localStorage.length;i++)
 	{
 		id = localStorage.key(i);
 		var todo = JSON.parse(localStorage.getItem(id));
@@ -24,13 +28,101 @@ $(document).ready(function(){
 		$("#myList").append(content);
 		
 	}
+	}
+	else{
+	for(i=0;i<localStorage.length;i++)
+	{
+		id = localStorage.key(i);
+		var todo = JSON.parse(localStorage.getItem(id));
+		title=todo.title;
+		description=todo.description;
+		priority=todo.priority;
+		done=todo.done;
+		
+		if(done==isDone)
+			content="<li id="+id+"><button class='done'>&#10004;</button><p class='title'>"+title+"</p><p class='description'>"+description+"</p><p class='priority'>"+priority+"</p><button class='close'>&#215;</button></li>";
+		
+		$("#myList").append(content);
+		
+	}
 	
+	}
+	
+	
+	
+	$(".close").click(function(){
+		console.log("element deleted");
+		id=$(this).parent().attr("id");
+		localStorage.removeItem(id);
+		$(this).parent().remove();
+		console.log("element deleted");
+	});
+	
+}
+
+function getpriority(){
+	
+	$("#myList").empty();
+	
+	for(j=5;j>0;j--){
+		for(i=0;i<localStorage.length;i++)
+		{
+			id = localStorage.key(i);
+			var todo = JSON.parse(localStorage.getItem(id));
+			title=todo.title;
+			description=todo.description;
+			priority=todo.priority;
+			done=todo.done;
+		
+			if(priority===j)
+			{
+				console.log(id);
+				content="<li id="+id+"><button class='done'>&#10004;</button><p class='title'>"+title+"</p><p class='description'>"+description+"</p><p class='priority'>"+priority+"</p><button class='close'>&#215;</button></li>";
+				$("#myList").append(content);
+			}
+		
+		}
+		
+	}
+	
+	
+	
+	$(".close").click(function(){
+		console.log("element deleted");
+		id=$(this).parent().attr("id");
+		localStorage.removeItem(id);
+		$(this).parent().remove();
+		console.log("element deleted");
+	});
+	
+}
+
+
+$(document).ready(function(){
+	
+	getTasks(-1);
+	
+	
+	$("#byPrio").click(getpriority);
+	
+	$("#notdone").click(function(){
+			getTasks(0);
+	});
 	
 	$("#add-todo").click(function(){
+		
+		
+		
 		time= new Date($.now());
 		priority=$("#priority").val();
+		$("#priority").val("");
+		
 		title=$("#title").val();
+		$("#title").val("");
+		
 		description=$("#task").val();
+		$("#task").val("");
+		
 		done=0;
 		priority=parseInt(priority);
 		
@@ -52,15 +144,11 @@ $(document).ready(function(){
 		console.log(i);
 		key="t"+i;
 		localStorage.setItem(key,JSON.stringify(todo));
+		
+		getTasks(-1);
 	
 	});
 	
-	$(".close").click(function(){
-		id=$(this).parent().attr("id");
-		localStorage.removeItem(id);
-		$(this).parent().remove();
-		console.log("element deleted");
-	});
 	
 	
 });
